0f884e38a634ec18ba0082e0978c8af2c9404074
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-03-09 13:40:30 +0100

    Add progress bar to backup tool
    Also improve error handling an replace the notifications with information boxes.

--- ansible/roles/specchio_backup_tool/files/specchio_backup_tool.sh
+++ ansible/roles/specchio_backup_tool/files/specchio_backup_tool.sh
@@ -3,14 +3,14 @@
 DIALOG_TITLE="SPECCHIO Backup Tool"
 
 ACTION=$(echo -e "Backup\nRestore" | zenity --list \
-	                                    --text "Do you want to create a backup or restore an existing one?" \
-                                            --column "Action" \
-	                                    --title "${DIALOG_TITLE}")
+					    --text "Do you want to create a backup or restore an existing one?" \
+					    --column "Action" \
+					    --title "${DIALOG_TITLE}")
 
 function backup {
     zenity --question \
-           --text "Are you sure you want to backup the database?" \
-           --title "${DIALOG_TITLE}" \
+	   --text "Are you sure you want to backup the database?" \
+	   --title "${DIALOG_TITLE}" \
 	   --width 380
 
     if [[ $? -eq 0 ]]; then
@@ -24,12 +24,18 @@ function backup {
 	     --quick \
 	     --routines \
 	     --triggers \
-	    | gzip -9 -c > SPECCHIO_$(date +%Y%m%d_%H%M%S).sql.gz
+	    | gzip -9 -c > SPECCHIO_$(date +%Y%m%d_%H%M%S).sql.gz \
+	    | zenity \
+		  --progress \
+		  --pulsate \
+		  --auto-close \
+		  --auto-kill \
+		  --text "This could take a while."
 
 	if [[ $? -eq 0 ]]; then
-	    zenity --notification --text "Backup was successful"
+	    zenity --info --width 380 --text "Backup was successful"
 	else
-	    zenity --notification --text "Backup failed with return code $?"
+	    zenity --info --width 380 --text "Backup failed with return code $?"
 	fi
     else
 	exit 0
@@ -40,25 +46,32 @@ function restore {
     local FILENAME="$(zenity --file-selection)"
 
     if [[ ! -f ${FILENAME} ]]; then
-        zenity --error \
-               --text "File does not exist!" \
-               --title "${DIALOG_TITLE}" \
-       	       --width 380
+	zenity --error \
+	       --text "File does not exist!" \
+	       --title "${DIALOG_TITLE}" \
+	       --width 380
 	exit 1
     fi
 
     zenity --warning \
-           --text "This will delete all the data of the current database (including users and passwords). Are you sure you want to restore the database from the specified dump?" \
-           --title "${DIALOG_TITLE}" \
+	   --text "This will delete all the data of the current database (including users and passwords). Are you sure you want to restore the database from the specified dump?" \
+	   --title "${DIALOG_TITLE}" \
 	   --width 380
 
     if [[ $? -eq 0 ]]; then
-	gunzip -c $FILENAME | sudo mysql
+	gunzip -c "${FILENAME}" \
+	    | sudo mysql \
+	    | zenity \
+		  --progress \
+		  --pulsate \
+		  --auto-close \
+		  --auto-kill \
+		  --text "This could take a while."
 
 	if [[ $? -eq 0 ]]; then
-	    zenity --notification --text "Restore was successful"
+	    zenity --info --width 380 --text "Restore was successful"
 	else
-	    zenity --notification --text "Restore failed with return code $?"
+	    zenity --info --width 380 --text "Restore failed with return code $?"
 	fi
     else
 	exit 0
